6b898d82c09b483d950b2d8cc5a93b870ff10365
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-08-31 18:59:42 +0500

    Initial project setup and auth service

---
+++ auth/src/index.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(json());
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log('connected to mongodb');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Auth server is listening on the port 3000');
+  });
+};
+
+start();
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { body, validationResult } from 'express-validator';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 to 20 characters'),
+  ],
+
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    console.log('body received = ', req.body);
+    if (!errors.isEmpty()) {
+      return next(new RequestValidationError(errors.array()));
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log('email in use');
+      throw new BadRequestError('Email already in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
+  }
+);
+
+export { router as signUpRouter };
